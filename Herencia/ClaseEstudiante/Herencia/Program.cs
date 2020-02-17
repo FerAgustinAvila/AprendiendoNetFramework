dcc0b0d898645b3858e85fc32084a7ffb5724de7
FerAgustinAvila <fernandoa_avila@hotmail.com>
2020-02-17 15:54:37 -0300

    Inheritance

---
+++ Herencia/ClaseEstudiante/Herencia/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Carlos", 38);
+            p.Saludar();            
+
+            Empleado e = new Empleado("John", 29, "Ventas", 10000);
+            e.Saludar();
+            e.Trabajar();
+            Console.WriteLine(e);
+
+            Estudiante es = new Estudiante("Jane", 18, 9.5, "8");
+            es.Saludar();
+            es.IrAClase();
+            es.Estudiar();
+        }
+    }
+}
---
+++ Herencia/Herencia/Herencia/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Herencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Carlos", 38);
+            p.Saludar();            
+
+            Empleado e = new Empleado("John", 29, "Ventas", 10000);
+            e.Saludar();
+            e.Trabajar();
+            Console.WriteLine(e);
+        }
+    }
+}
