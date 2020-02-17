9725c1d786f12737a34cf72ba39c75dec2d421d2
FerAgustinAvila <fernandoa_avila@hotmail.com>
2020-02-17 20:03:06 -0300

    Structures

---
+++ Estructurass/Estructuras/Estructuras/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estructuras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vector v1 = new Vector(1, 2, 3);
+            Vector v2 = new Vector(4, 5, 6);
+
+            Console.WriteLine("Módulo vector 1: {0}", v1.CalcularModulo());
+            Console.WriteLine("Módulo vector 2: {0}", v2.CalcularModulo());
+
+            Vector resultado = v1.Suma(v2);
+            Console.WriteLine(v1);
+            Console.WriteLine(v2);
+            Console.WriteLine(resultado);
+        }
+    }
+}
---
+++ Estructurass/NumerosComplejos/NumerosComplejos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosComplejos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Complejo c1 = new Complejo(3, 4);
+            Complejo c2 = new Complejo(-0.5, 2);
+
+            Console.WriteLine(c1);
+            Console.WriteLine(c2);
+            Complejo resultado = c1.Suma(c2);
+            Console.WriteLine(resultado);
+        }
+    }
+}
