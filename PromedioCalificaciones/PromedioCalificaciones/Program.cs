b7c5afab00cc0738044c713d6dbb4e9d336f42df
FerAgustinAvila <fernandoa_avila@hotmail.com>
2020-02-17 10:27:32 -0300

    Second Commit
    Second Commit

---
+++ PromedioCalificaciones/PromedioCalificaciones/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PromedioCalificaciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número de calificaciones a ingresar: ");
+            int num = int.Parse(Console.ReadLine());
+
+            int[] calificaciones = new int[num];
+
+            double suma = 0;
+            for(int i = 0; i < calificaciones.Length; i++)
+            {
+                Console.Write("Calificación {0}: ", i + 1);
+                calificaciones[i] = int.Parse(Console.ReadLine());
+                suma += calificaciones[i];
+            }
+
+            double promedio = suma / calificaciones.Length;
+            Console.WriteLine("El promedio es: {0}", promedio);        
+        }
+    }
+}
---
+++ SintaxisArreglos/SintaxisArreglos/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SintaxisArreglos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Sintaxis 1
+            int[] arreglo = new int[5];
+            arreglo[0] = 1;
+            arreglo[1] = 0;
+            arreglo[2] = 20;
+            arreglo[3] = -4;
+            arreglo[4] = 8;
+
+            Console.WriteLine(arreglo[4]);
+
+            // Sintaxis 2
+            char[] vocales = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            Console.WriteLine(vocales[2]);
+
+            // Sintaxis 3
+            double[] numeros = { 1.1, 2.2, 3.4, 5.8 };
+            Console.WriteLine("Tamaño de números: {0}", numeros.Length);
+
+            // mostrar último elemento
+            Console.WriteLine(numeros[numeros.Length - 1]);
+        }
+    }
+}
